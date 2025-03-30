e465027c1125ce354877a310151358223a55b901
tobiasgrosser <tobiasgrosser@users.noreply.github.com>
2025-03-30 12:23:57 +0000

    Update main to output generated at c9acb8d

---
+++ armv9/Armv9/Trace.lean
@@ -0,0 +1,105 @@
+import Armv9.Arith
+
+set_option maxHeartbeats 1_000_000_000
+set_option maxRecDepth 10_000
+set_option linter.unusedVariables false
+set_option match.ignoreUnusedAlts true
+
+open Sail
+
+noncomputable section
+
+namespace Armv9.Functions
+
+open signal
+open option
+open exception
+open arm_acc_type
+open __InstrEnc
+open WFxType
+open VFPNegMul
+open VCGTtype
+open VCGEType
+open VBitOps
+open VBitOp
+open VARange
+open Unpredictable
+open TranslationStage
+open TimeStamp
+open TMFailure
+open TLBIOp
+open TLBIMemAttr
+open TLBILevel
+open TGx
+open SystemHintOp
+open Signal
+open ShiftType
+open Shareability
+open SecurityState
+open SVECmp
+open SRType
+open SMEExceptionType
+open SDFType
+open RestrictType
+open Register
+open Regime
+open ReduceOp
+open PrivilegeLevel
+open PrefetchHint
+open PSTATEField
+open PGSe
+open PASpace
+open PARTIDspaceType
+open OpType
+open MoveWideOp
+open MemType
+open MemTagType
+open MemOp
+open MemAtomicOp
+open MOPSStage
+open MBReqTypes
+open MBReqDomain
+open LogicalOp
+open InterruptID
+open InstrSet
+open ImmediateOp
+open GPCF
+open GCSInstruction
+open Feature
+open Fault
+open FPUnaryOp
+open FPType
+open FPRounding
+open FPMaxMinOp
+open FPExc
+open FPConvOp
+open ExtendType
+open ExceptionalOccurrenceTargetState
+open Exception
+open ErrorState
+open DeviceType
+open DescriptorType
+open DSBAlias
+open CrossTriggerIn
+open CountOp
+open Constraint
+open CompareOp
+open CacheType
+open CachePASpace
+open CacheOpScope
+open CacheOp
+open BranchType
+open Barrier
+open ArchVersion
+open AccessType
+open ATAccess
+
+def enable_tracing (_ : Unit) : Unit :=
+  ()
+
+def disable_tracing (_ : Unit) : Unit :=
+  ()
+
+def is_tracing (_ : Unit) : Bool :=
+  false
+
