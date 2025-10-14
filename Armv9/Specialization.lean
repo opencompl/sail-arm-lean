ffda274f9dc8f47dd90089bb35a9f916d2405c49
tobiasgrosser <tobiasgrosser@users.noreply.github.com>
2025-10-14 04:22:28 +0000

    Update main to output generated at 34adf48

--- Armv9/Specialization.lean
+++ Armv9/Specialization.lean
@@ -3,8 +3,10 @@ import Armv9.Defs
 
 namespace Sail
 
+@[simp_sail]
 def sailTryCatch (e : SailM α) (h : exception → SailM α) : SailM α := PreSail.sailTryCatch e h
 
+@[simp_sail]
 def sailThrow (e : exception) : SailM α := PreSail.sailThrow e
 
 abbrev undefined_unit (_ : Unit) : SailM Unit := PreSail.undefined_unit ()
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ is still work-in-progress. As a result, our semantics are still full of warnings
 and errors. Similarly, our output is not yet polished for readability.
 # Statistics
 
-Lines: 1188  
+Lines: 1250  
 Definitions: 145  
 Inductive definitions: 7  
 Abbreviations: 47  
